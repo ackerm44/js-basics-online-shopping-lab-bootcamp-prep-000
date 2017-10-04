c25aa8f8ac8d5bcc6261487bd394f0bad6ad17e8
Amanda Ackerman <ackerm44@gmail.com>
2017-10-04 13:03:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,12 +64,12 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber) {
-    console.log("Sorry, we don't have a credit card on file for you.");
-  } else {
     console.log(`Your total cost is $${total}, which will be charged to the card ${cardNumber}.`)
     for (var i = 0; i < cart.length; i++) {
       cart = [];
     }
     return cart;
+  } else {
+    console.log("Sorry, we don't have a credit card on file for you.");
   }
 }
