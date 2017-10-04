115a611c0e06acc26cedcba3c51141411d89729c
Amanda Ackerman <ackerm44@gmail.com>
2017-10-04 12:36:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function addToCart(item) {
   var obj = new Object({});
   obj[item] = itemPrice;
   cart.push(obj);
-  console.log(item + " has been added to your cart.");
+  console.log(`${item} has been added to your cart.`);
   return cart;
 }
 
