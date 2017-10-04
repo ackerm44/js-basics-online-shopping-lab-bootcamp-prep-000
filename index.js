329f5ccb068434628fc98e5f8b906a96a9d81f45
Amanda Ackerman <ackerm44@gmail.com>
2017-10-04 14:54:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,11 +37,9 @@ function viewCart() {
               string += "."            }
           } else {
               string += `${key} at $${cart[i][key]}`;
-              if (i == cart.length - 3) {
-                string += ", "
-              } else if (i == cart.length - 2) {
-                string += `, and `;
-              } else if (i == cart.length - 1) {
+              if (i < cart.length -1) {
+                string += ", ";
+              } else {
                 string += "."
               }
             }
