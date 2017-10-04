dbfd87f3ef23e1a31ec7c803e5b91a5ccc278c8f
Amanda Ackerman <ackerm44@gmail.com>
2017-10-04 13:10:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber > 10000000) {
-    var total = total(); 
+    total(); 
     console.log(`Your total cost is $${total}, which will be charged to the card ${cardNumber}.`)
     for (var i = 0; i < cart.length; i++) {
       cart = [];
