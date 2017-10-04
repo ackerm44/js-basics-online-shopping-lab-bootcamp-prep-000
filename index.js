494fa42cc154c10423b540666fcb038c2592d02b
Amanda Ackerman <ackerm44@gmail.com>
2017-10-04 13:36:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-  var itemInCart = false;
+  var itemInCart == false;
 
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].hasOwnProperty(item)) {
